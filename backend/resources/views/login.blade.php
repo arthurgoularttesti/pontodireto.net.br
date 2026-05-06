96078cd8e9ad1857c5310b0e22e5f12b1e8ea6ba
arthurgoularttesti <arthurgoulart@gmail.com>
2026-05-06 13:25:41 -0300

    favicon

--- backend/resources/views/login.blade.php
+++ backend/resources/views/login.blade.php
@@ -7,6 +7,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&amp;display=swap" rel="stylesheet"/>
 	<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet"/>
 	<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet"/>
+	<link rel="icon" type="image/x-icon" href="{{ asset('favicon/favicon.ico') }}">
 	<script id="tailwind-config">
 			tailwind.config = {
 				darkMode: "class",
